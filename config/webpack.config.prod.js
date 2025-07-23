c63fd67f884f65d0515112533c72e034b42a9e1e
Michael Rustanto <michael.rustanto@bhinneka.com>
2025-07-24 00:37:21 +0700

    Revert Local Dependencies & react-dev-utils to 10.0.0.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -24,7 +24,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
-const publicPath = paths.publicUrlOrPath || './';
+const publicPath = paths.servedPath;
 // Some apps do not use client-side routing with pushState.
 // For these, "homepage" can be set to "." to enable relative asset paths.
 const shouldUseRelativeAssetPaths = publicPath === './';
